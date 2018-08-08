8e764909d874eebadae3075e6712bda8158d6752
Rodrigo Meira de Andrade <rodrigormda@hotmail.com>
2018-08-07 23:31:18 -0300

    get snap from client and save it

--- howisyourday.py
+++ howisyourday.py
@@ -10,17 +10,29 @@ import glob
 import subprocess
 import socket
 
+import numpy as np
+import cv2
+
+
 
 app = Flask(__name__)
-
-@app.route('/testeok')
-def testeok():
-    return "Deu boa irmão!!!!"
+g_imgCounter = 0
 
 @app.route('/howisurday')
 def todo_twitt():
     now = datetime.datetime.now()
     return render_template('HowIsUrDay.html', items=now)
 
+@app.route('/howisurday_user_snap', methods = ['POST'])
+def get_user_snap():
+    global g_imgCounter
+    file = request.files['webcam']
+    image = cv2.imdecode(np.fromstring(file.stream.read(),np.uint8), cv2.IMREAD_UNCHANGED)
+    cv2.imwrite("{}.jpg".format(g_imgCounter),image)
+    g_imgCounter+=1
+
+    return ""
+
 if __name__ == "__main__":
+    app.debug = True
     app.run(host=socket.gethostbyname(socket.gethostname()), threaded=True, ssl_context='adhoc')
